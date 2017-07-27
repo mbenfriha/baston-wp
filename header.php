0ca569126b4c6b70fda28533c155bfe780d009bc
mbenfriha <benfriha.mohamed@gmail.com>
2017-07-27 14:46:26 +0200

    jquery link

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
 
 
-    <script type="text/javascript" src="<?php echo get_template_directory_uri () ?>/inc/js/jquery.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
     <script src="<?php echo get_template_directory_uri () ?>/inc/js/materialize.min.js"></script>
 
